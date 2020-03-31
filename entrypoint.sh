c4105fee26aaad317caa23a1647d67e0978b2c69
zhuYe <loveqianool@users.noreply.github.com>
2020-03-31 23:49:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 day=$(/bin/date +%Y-%m-%d-%H-%M);
 mkdir $DATA_FOLDER/backup;
 sqlite3 $DATA_FOLDER/db.sqlite3 ".backup '$DATA_FOLDER/backup/db.sqlite3-$day'"
-zip -m -P $PASSWD $DATA_FOLDER/backup/db.sqlite3-$day.zip $DATA_FOLDER/backup/db.sqlite3-$day
+zip -m -P $PASSWORD $DATA_FOLDER/backup/db.sqlite3-$day.zip $DATA_FOLDER/backup/db.sqlite3-$day
 find $DATA_FOLDER/backup/* -type f -mtime +7 -exec rm -rf {} \;
 rclone sync $DATA_FOLDER/backup $REMOTE:/bitwarden_backup -v -P
